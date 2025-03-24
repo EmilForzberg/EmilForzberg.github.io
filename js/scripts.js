a88bcf7bbc4b9b46cef89fc0eb7fdc3c4f24065e
Emil Forsberg <forsbergtaggarn@hotmail.com>
2025-03-24 10:57:30 +0100

    Klistrade in kod från index.html, det är mestadels kod för att behålla samma temma som första sidan.
    Jag vet inte om bilden försvinner, känns som det är då och då.
    
    Script-filen tror jag är rätt länkad, det ligger nu separat i en lång script-fil.

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,21 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+
+
+    // Produktinfo
+    if (window.location.pathname.includes("product.html")) {
+    const params = new URLSearchParams(window.location.search);
+    const name = params.get('name');
+    const image = params.get('image');
+    const description = params.get('description');
+
+    // Fyll i på sidan
+    if (name) document.getElementById('product-title').textContent = name;
+    if (image) document.getElementById('product-image').src = image;
+    if (description) document.getElementById('product-description').textContent = description;
+    }
+
+
+    
